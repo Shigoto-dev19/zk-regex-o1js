d79b0c65a9150d3e7eefbcb200c79e9a7e12ff5e
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2024-03-27 12:06:39 +0100

    Add tests for the mina zk-regex example

--- src/circuit.test.ts
+++ src/circuit.test.ts
@@ -5,6 +5,7 @@ import {
   simpleRegex, 
   emailRegex, 
   base64Regex, 
+  minaRegex,
 } from './examples';
 
 //TODO use `Bytes.fromString(input).toFields()` instead
@@ -23,7 +24,7 @@ function padString(str: string, paddedBytesSize?: number): Field[] {
 
 // 1=(a|b) (2=(b|c)+ )+d
 describe("Simple Regex", () => {
-  it("should accept valid input: case 1", async () => {
+  it("should accept valid input: case 1", () => {
     const input = "1=a 2=b d";
     const paddedStr = padString(input, 64);
 
@@ -587,5 +588,129 @@ describe("Base64 Regex", () => {
     
     const isValid = emailRegex(paddedStr);
     expect(isValid).toEqual(Field(0))
+  });
+});
+
+// (mina|MINA)+
+// This example demonstrates how zk-regex counts occurrences of patterns.
+describe("Mina Regex", () => {
+  it("should find occurence of 1 'mina' pattern: case 1", () => {
+    const input = "mina";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(1));
+  });
+
+  it("should find occurence of 1 'mina' pattern: case 2", () => {
+    const input = "stamina";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(1));
+  });
+
+  it("should find occurence of 1 'mina' pattern: case 3", () => {
+    const input = "The terminal station is nearby";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(1));
+  });
+
+  it("should find occurence of 2 'mina' patterns: case 1", () => {
+    const input = "I need stamina to reach the faraway terminal";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(2));
+  });
+
+  it("should find occurence of 2 'mina' patterns: case 2", () => {
+    const input = "Amina is dominating the tournament";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(2));
+  });
+
+  it("should find occurence of 3 'mina' patterns", () => {
+    const input = "Amina is dominating the tournament with her stamina";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(3));
+  });
+
+  it("should find occurence of 1 'MINA' pattern: case 1", () => {
+    const input = "MINA is a succint blockchain";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(1));
+  });
+
+  it("should find total occurence of 2 'MINA' or 'mina' pattern", () => {
+    const input = "Is MINA minable?";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(2));
+  });
+
+  it("should find total occurence of 3 'MINA' or 'mina' pattern", () => {
+    const input = "MINAminaMINA";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(3));
+  });
+
+  it("should find total occurence of 4 'MINA' or 'mina' pattern", () => {
+    const input = "Be luminary and illuminate, don't discriminate or eliminate";
+    const paddedStr = padString(input);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(4));
+  });
+
+  it("should find no valid pattern: case 1", () => {
+    const input = "Mina is a western name!";
+    const paddedStr = padString(input, 64);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(0));
+  });
+
+  it("should find no valid pattern: case 2", () => {
+    const input = "lol";
+    const paddedStr = padString(input, 64);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(0));
+  });
+
+  it("should find no valid pattern: case 3", () => {
+    const input = "MiNa doesn't count!";
+    const paddedStr = padString(input, 64);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(0));
+  });
+
+  it("should find no valid pattern: case 4", () => {
+    const input = "The word miniature is close enough";
+    const paddedStr = padString(input, 64);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(0));
+  });
+
+  it("should find no valid pattern: case 5", () => {
+    const input = "";
+    const paddedStr = padString(input, 64);
+    
+    const isValid = minaRegex(paddedStr);
+    expect(isValid).toEqual(Field(0));
   });
 });
